675ac4b854d5e6c759e6cd714a1b143e2c78e773
kranthi726 <105335244+kranthi726@users.noreply.github.com>
2022-05-26 17:16:13 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
 const cors = require("cors");
+
 const app = express();
+
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8082"
 };
+
 app.use(cors(corsOptions));
+
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json());  /* bodyParser.json() is deprecated */
+
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-// set up database 
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
 const db = require("./app/models");
+
 db.sequelize.sync();
-// for devel to recreate each time database 
+// // drop the table if it already exists
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
 // });
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
-require("./app/routes/tutorial.routes")(app);
+
+require("./app/routes/turorial.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
