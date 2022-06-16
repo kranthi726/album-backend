ad03b28916dabd0d388022b6bdc1405171a5a43d
Greeshma77777 <105335632+Greeshma77777@users.noreply.github.com>
2022-06-16 16:21:39 -0500

    Add files via upload
    Added routes to controllers to the backend AWS Depoyement

--- artist.controller.js
+++ artist.controller.js
@@ -1,8 +1,8 @@
 const db = require('../models')
-const Track = db.track
+const Artist = db.artist
 const Op = db.Sequelize.Op;
 
-//Create and Save New Track
+//Create and Save New artist
 exports.create = (req, res) =>{
     if(!req.body.title){
         res.status(400).send({
@@ -10,23 +10,21 @@ exports.create = (req, res) =>{
         })
         return
     }
-    //Create the Track
-    const track = {
+    //Create the artist
+    const artist = {
         title:req.body.title,
         description: req.body.description,
-        published: req.body.published ? req.body.published : false,
-        album:req.body.album,
+        published: req.body.published ? req.body.published : false
     }
-    //save the track
-    Track.create(track)
+    //save the artist
+    Artist.create(artist)
     .then(data => {
         res.send(data);
-        alert(data);
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while creating the Track."
+            err.message || "Some error occurred while creating the artist."
         });
       });
 
@@ -35,73 +33,73 @@ exports.create = (req, res) =>{
 exports.findAll = (req, res)=>{
     const title = req.query.title;
     var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-    Track.findAll({where:condition})
+    Artist.findAll({where:condition})
     .then(data => {
         res.send(data);
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving Track."
+            err.message || "Some error occurred while retrieving artist."
         });
       });
 }
 
 exports.findOne =(req,res)=>{
     const id = req.params.id;
-    Track.findByPk(id)
+    Artist.findByPk(id)
     .then(data => {
         if (data) {
           res.send(data);
         } else {
           res.status(404).send({
-            message: `Cannot find Track with id=${id}.`
+            message: `Cannot find artist with id=${id}.`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error retrieving Track with id=" + id
+          message: "Error retrieving artist with id=" + id
         });
       });
 }
 
 exports.update = (req, res) =>{
     const id = req.params.id;
-    Track.update(req.body,{
+    Artist.update(req.body,{
         where: { id: id }
     }).then(num => {
         if (num == 1) {
           res.send({
-            message: "Track was updated successfully."
+            message: "artist was updated successfully."
           });
         } else {
           res.send({
-            message: `Cannot update Track with id=${id}. Maybe Track was not found or req.body is empty!`
+            message: `Cannot update artist with id=${id}. Maybe artist was not found or req.body is empty!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating Track with id=" + id
+          message: "Error updating artist with id=" + id
         });
       });
 }
-// Delete a Track with the specified id in the request
+// Delete a artist with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
   
-    Track.destroy({
+    Artist.destroy({
       where: { id: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "Track was deleted successfully!"
+            message: "Artist was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete Track with id=${id}. Maybe Tutorial was not found!`
+            message: `Cannot delete artist with id=${id}. Maybe artist was not found!`
           });
         }
       })
@@ -113,31 +111,31 @@ exports.delete = (req, res) => {
   };
   // Delete all Tracks from the database.
 exports.deleteAll = (req, res) => {
-    Track.destroy({
+    Artist.destroy({
       where: {},
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} Track were deleted successfully!` });
+        res.send({ message: `${nums} artist were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all Track."
+            err.message || "Some error occurred while removing all artist."
         });
       });
   };
   
   // find all published Tutorial
   exports.findAllPublished = (req, res) => {
-    Track.findAll({ where: { published: true } })
+    Artist.findAll({ where: { published: true } })
       .then(data => {
         res.send(data);
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving tutorials."
+            err.message || "Some error occurred while retrieving artists."
         });
       });
   };
